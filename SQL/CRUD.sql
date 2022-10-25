4f6b0bc3f0efd2bac0d478d4ad78e60ae6f7e1c4
Ted Janney <janneyt@oregonstate.edu>
2022-10-25 13:38:48 -0400

    Added country's sqls

--- SQL/CRUD.sql
+++ SQL/CRUD.sql
@@ -54,6 +54,56 @@ USE @database;
 DELETE from Characters where idCharacter = `${id passed from html page}`
 COMMIT;
 
+-- Display country sqls full page
+START TRANSACTION;
+USE @database;
+
+SELECT idCountry, countryName, sizeInKm, population from Characters;
+
+COMMIT;
+
+-- Retrieve one specific character sql by id
+START TRANSACTION;
+USE @database;
+
+SELECT idCountry, countryName, sizeInKm, population from Characters
+WHERE idCountry = `${HTML page will send over id to be retrieved}`;
+
+COMMIT;
+
+-- Retrieve one specific character sql by name
+START TRANSACTION;
+USE @database;
+
+SELECT idCountry, countryName, sizeInKm, population from Characters 
+WHERE countryName = `${countryName passed from html page}`;
+
+COMMIT;
+
+-- Create Character sql
+START TRANSACTION;
+use @database;
+
+INSERT INTO 'Countries' (countryName, sizeInKm, population) VALUES (`${This will be a name}`,`${This will be a size in integers}`, `${This will be a population in integers}`)
+
+COMMIT;
+
+-- Update Character sql
+START TRANSACTION;
+use @database;
+
+UPDATE `Countries`
+SET countryName = `${new name}`, sizeInKm = `${new size}`, population = `${population}`
+WHERE idCountry = `${id from html page}`
+COMMIT;
+
+-- Delete Character by id SQL
+START TRANSACTION;
+USE @database;
+
+DELETE from country where idCountry = `${id passed from html page}`
+COMMIT;
+
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
