a967f787f380e334cc579c3ba71b13458f7e7e6f
Ted Janney <janneyt@oregonstate.edu>
2022-10-26 09:49:59 -0400

    Changed the USE statements to SQL variable

--- SQL/DDL_2.sql
+++ SQL/DDL_2.sql
@@ -3,18 +3,18 @@
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+DECLARE @database as varchar(45)='cs340_janneyt';
+-- -----------------------------------------------------
+-- Schema cs340_dempsjam
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS @database;
 
 -- -----------------------------------------------------
 -- Schema cs340_dempsjam
 -- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `cs340_dempsjam` ;
-
--- -----------------------------------------------------
--- Schema cs340_dempsjam
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `cs340_dempsjam` ;
+CREATE SCHEMA IF NOT EXISTS @database;
 SHOW WARNINGS;
-USE `cs340_dempsjam` ;
+USE @database;
 
 -- -----------------------------------------------------
 -- Table `Games`
