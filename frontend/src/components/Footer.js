8f2260364550482799d78aaad16e72e64e5e039f
JA-Dempsey <andrew@dempseycentral.com>
2022-11-03 19:31:24 -0400

    Added footer with index link

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,14 +1,12 @@
-import { useNavigate, Link } from 'react-router-dom';
 import Button from './Button';
 
 const Footer = () => {
-
-    const navigate = useNavigate();
-
     return (
         <footer>
             <p>Lost? Just go back to the Index page.</p>
-            <Button onClick={() => navigate("/")}>Back to Index</Button>
+            <a href="/"><Button>Back to Index</Button></a>
         </footer>
     );
 };
+
+export default Footer;
