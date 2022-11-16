601af37492823a71da46829f2cb07ea57cf669a0
Ted Janney <janneyt@oregonstate.edu>
2022-11-17 02:48:26 +1000

    Current Form.js

--- frontend/src/components/Forms/Form.js
+++ frontend/src/components/Forms/Form.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Button from '../Button';
 import FormInput from './FormInput';
 import FormSelect from './FormSelect';
@@ -6,11 +6,14 @@ import FormSelect from './FormSelect';
 
 import { useNavigate } from 'react-router-dom';
 
-const Form = ({ submitText="Submit", inputState}) => {
+const Form = ({ submitText="Submit", inputState, parent_callback}) => {
     
-    const [inputFields, setInputFields] = useState(inputState)
-    
+    const [inputFields, setInputFields] = useState([{}])
 
+    useEffect(() => {
+        setInputFields(inputState);
+    }, [inputState]);
+    
     const handleFormChange = (index, e) => {
         e.preventDefault()
         let input = [...inputFields];
@@ -18,11 +21,16 @@ const Form = ({ submitText="Submit", inputState}) => {
         setInputFields(input);
     }
 
+    const handleSubmit = () => {
+
+        parent_callback(inputFields);
+    }
+
     const formFields = inputFields.map((row, index) => {
         // Check to see what type of input to place into the form
         // Currently this can be type="text" or "select"
         // Defaults to "text"
-
+        console.log("Row", row)
         if (row.type == "text") {
             return <FormInput key={index} inputObj={row} onChange={handleFormChange} index={index} />
         };
@@ -35,11 +43,11 @@ const Form = ({ submitText="Submit", inputState}) => {
     const navigate = useNavigate();
 
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             {formFields}
             <Button type="submit">{submitText}</Button>
             <Button onClick={() => {
-                navigate(-1);
+                navigate("/items");
                 }}>Cancel</Button>
         </form>
     );
