6968751894d9187afb8e89d6ef5d2a2fca68256b
JA-Dempsey <andrew@dempseycentral.com>
2022-11-16 22:09:56 -0500

    Skeleton to get data from the Form

--- frontend/src/pages/items/AddItem.js
+++ frontend/src/pages/items/AddItem.js
@@ -1,11 +1,29 @@
 import Form from '../../components/Forms/Form';
 import { addFormContents } from '../../data/itemData';
+import { useEffect, useState } from 'react';
 
 function AddItem() {
+
+    const [submitData, setSubmitData] = useState();
+    
+    const prepareAddData = (e) => {
+        e.preventDefault();
+        
+        const form = e.target
+        for (const item of form) {
+            if (item.nodeName == "INPUT")
+            console.log(item)
+        }
+        console.log("TEST",form);
+    };
+
+
+
+
     return (
         <div className="content">
             <h1>Add Item Page</h1>
-            <Form submitText="Save" inputState={addFormContents} />
+            <Form submitText="Save" inputState={addFormContents} onSubmit={prepareAddData} />
         </div>
     )
 }
