a4fed854c67be3c50fc5460e70025e3c1199ed82
JA-Dempsey <andrew@dempseycentral.com>
2022-11-17 17:12:20 -0500

    Basic setup for inserting to db for AddTranslation

--- frontend/src/pages/translations/AddTranslation.js
+++ frontend/src/pages/translations/AddTranslation.js
@@ -1,11 +1,29 @@
 import Form from '../../components/Forms/Form';
 import { addFormContents } from '../../data/translationData';
 
+import { useEffect, useState, useRef } from 'react';
+import { prepareFormData } from '../../functions/submitFunctions.js';
+import { insertData } from '../../axios/crud.js';
+import { useNavigate } from 'react-router-dom';
+
 function AddTranslation() {
+
+    const navigate = useNavigate();
+
+    const dataRef = useRef({});
+    const submitData = useRef({"columns":[], "values": []});
+
+    const prepareAddData = (e) => {
+        e.preventDefault();
+        prepareFormData(dataRef, submitData);
+        insertData("Items", submitData.current);
+        navigate("/items")
+    }; 
+
     return (
         <div className="content">
             <h1>Add Translation Page</h1>
-            <Form submitText="Save" inputState={addFormContents} />
+            <Form submitText="Save" inputState={addFormContents} onSubmit={prepareAddData} refDict={dataRef} />
         </div>
     )
 }
