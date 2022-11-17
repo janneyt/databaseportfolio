c7dd0d4f0b0dda55e612c869bbde006ebbbed56b
JA-Dempsey <andrew@dempseycentral.com>
2022-11-16 20:42:20 -0500

    class -> className

--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -2,7 +2,7 @@ export function SearchBar() {
     return (
         <>
 
-            <div class="topnav">
+            <div className="topnav">
                 <br />
                 <input type="text" placeholder="Search by player name or game name.."></input>
                 <input type="submit"></input>
