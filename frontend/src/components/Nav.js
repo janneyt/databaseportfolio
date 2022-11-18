5af0928851f84ac72e460200cf8d2ef6484b2116
JA-Dempsey <andrew@dempseycentral.com>
2022-11-18 01:23:22 -0500

    Placeholder nav func

---
+++ frontend/src/components/Nav.js
@@ -0,0 +1,16 @@
+import { NavLink } from 'react-router-dom';
+import Button from './Button';
+import { useNavigate } from 'react';
+
+const Nav = () => {
+    const navigate = useNavigate();
+    return (
+        <>
+            <nav>
+                
+            </nav>
+        </>
+    );
+};
+
+export default Nav;
